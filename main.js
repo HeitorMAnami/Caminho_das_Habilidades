e69e7dc6affda808836a0e1a261504f9f5c517a0
HeitorMAnami <heitor.anami@escola.pr.gov.br>
2026-09-02 15:00:09 -0300

    main.js

--- main.js
+++ main.js
@@ -12,4 +12,10 @@ const desafio =["Resolver problemas lógicos",
             alert("Digite Seu Nome Antes De Começar")
             return;
         }
+     const numeros = Math.floor(Math.randon() * desafios.length);
+    const desafio = desafios[numeros];
+
+    document.getElementById("resposta").innerHTML=`
+    <h2> Olá ${name}</h2>`
+    `<h2> Seu Desafio é: ${desafio}</h2>`
     }
