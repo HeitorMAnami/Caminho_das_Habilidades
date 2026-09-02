aa448eb7b7d18a83c4f2e05fc345d19c63db68e3
HeitorMAnami <heitor.anami@escola.pr.gov.br>
2026-09-02 14:19:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const desafio =["Resolver problemas lógicos",
+     "Aprender Novas Funções",
+     "Encontrar Padrões",
+     "Criar Novas soluções",
+     "Analisar erros"
+    ];
 
+    function inicarDesafio(){
+        const name = document.getElementById("name").value;
+
+        if (name === ""){
+            alert("Digite Seu Nome Antes De Começar")
+            return;
+        }
+    }
